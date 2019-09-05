ac73478f7ee8291f5f909c5b578a963b8bde7c2c
NecronizerFTW <varun.h.khatri@gmail.com>
2019-09-05 19:08:09 +0530

    Doubly Linked List ADT. Closes #1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from DoublyLinkedList.DoublyLinkedList import DoublyLinkedList
+
+linked_list = DoublyLinkedList(3)
+linked_list.push(1)
+linked_list.push(2)
+linked_list.push(3)
+linked_list.dequeue()
+linked_list.push(4)
+
+linked_list.display()
+linked_list.display_reverse()
