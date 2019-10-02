e842ed6fbc71fb38730de1c25e5afe2355699530
NecronizerFTW <varun.h.khatri@gmail.com>
2019-10-02 12:50:12 +0530

    Comments and Bug Fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from CPU.RoundRobin.RoundRobin import RoundRobin
 from Cache.FIFO.FIFO import FIFO
 from Cache.LRU.LRU import LRU
 
+# Initializing required variables and data structures.
 fifo_cache = None
 lru_cache = None
 fcfs_cpu = None
@@ -13,24 +14,29 @@ init_arrival_time = []
 print("WELCOME TO CPU AND CACHE ALGORITHMS SIMULATOR!")
 print("             - A Project by Varun Khatri (E064)")
 
+# Continuous loop for UI.
 while True:
 
     print("WHICH ALGORITHM DO YOU WISH TO EXECUTE FIRST?")
     print("0. EXIT THE PROGRAM\n1. CACHE ALGORITHMS\n2. CPU ALGORITHMS")
     choice = int(input("ENTER YOUR CHOICE: "))
 
+    # CASE 0: EXIT THE PROGRAM
     if choice is 0:
         break
 
+    # CASE 1: CACHE ALGORITHMS.
     elif choice is 1:
         print("WHICH ALGORITHM DO YOU WISH TO RUN FIRST?")
         print("1. LRU ALGORITHM\n2. FIFO ALGORITHM")
         algo_choice = int(input("ENTER YOUR CHOICE: "))
         pages = int(input("PLEASE ENTER NUMBER OF AVAILABLE PAGES: "))
 
+        # CASE 1.1: LRU ALGORITHM.
         if algo_choice is 1:
             lru_cache = LRU(total_length=pages)
 
+            # Loop for keeping the algorithm alive if user wants to.
             while True:
                 data = int(input("ENTER DATA TO ENTER: "))
                 lru_cache.set_value(data)
@@ -43,9 +49,11 @@ while True:
                 else:
                     print("YOU WISHED TO CONTINUE")
 
+        # CASE 1.2: FIFO ALGORITHM.
         elif algo_choice is 2:
             fifo_cache = FIFO(total_length=pages)
 
+            # Loop for keeping the algorithm alive if user wants to.
             while True:
                 data = int(input("ENTER DATA TO ENTER: "))
                 fifo_cache.set_value(value=data)
@@ -58,82 +66,115 @@ while True:
                 else:
                     print("YOU WISHED TO CONTINUE")
 
+        # ERROR CASE
         else:
             print("WRONG INPUT. TRY AGAIN")
+
+    # CASE 2: CPU ALGORITHMS.
     elif choice is 2:
         print("WHICH CPU ALGORITHM DO YOU WISH TO START?")
         print("1. FIRST COME FIRST SERVE\n2. ROUND ROBIN")
         algo_choice = int(input("ENTER YOUR CHOICE: "))
         no_of_processes = int(input("ENTER NUMBER OF PROCESSES: "))
 
+        # CASE 2.1: FCFS Algorithm.
         if algo_choice is 1:
-            fcfs_cpu = FCFS()
-            print("STARTING UP THE CPU.....")
-            fcfs_cpu.startup_cpu(no=no_of_processes)
 
-            print("INITIALIZE ARRIVAL TIMES: ")
-            for i in range(0, no_of_processes):
-                time = int(input("ENTER ARRIVAL TIME FOR PROCESS P{} :".format(i)))
-                init_arrival_time.append(time)
+            # Loop for keeping the algorithm alive if user wants to.
+            while True:
+                fcfs_cpu = FCFS()
+                print("STARTING UP THE CPU.....")
+                fcfs_cpu.startup_cpu(no=no_of_processes)
 
-            print("INITIALIZE BURST TIMES: ")
-            for i in range(0, no_of_processes):
-                time = int(input("ENTER BURST TIME FOR PROCESS P{} :".format(i)))
-                init_burst_time.append(time)
+                # Number of processes cannot be 0.
+                if no_of_processes is not 0:
 
-            fcfs_cpu.init_burst_time(burst_times=init_burst_time)
-            fcfs_cpu.init_arrival_time(arrival_times=init_arrival_time)
+                    # Arrival Times Input.
+                    print("INITIALIZE ARRIVAL TIMES: ")
+                    for i in range(0, no_of_processes):
+                        time = int(input("ENTER ARRIVAL TIME FOR PROCESS P{} :".format(i)))
+                        init_arrival_time.append(time)
 
-            fcfs_cpu.cpu_status()
-            print("STARTING TO COMPILE DATA...")
-            fcfs_cpu.compile()
+                    # Burst Times Input.
+                    print("INITIALIZE BURST TIMES: ")
+                    for i in range(0, no_of_processes):
+                        time = int(input("ENTER BURST TIME FOR PROCESS P{} :".format(i)))
+                        init_burst_time.append(time)
 
-            print("STATUS OF THE CPU AFTER COMPILING")
-            fcfs_cpu.cpu_status()
+                    fcfs_cpu.init_burst_time(burst_times=init_burst_time)
+                    fcfs_cpu.init_arrival_time(arrival_times=init_arrival_time)
 
-            confirmation = str(input("DO YOU WISH TO RE-ENTER VALUES AND COMPILE IT AGAIN? ENTER YES OR NO: "))
+                    # Compile Start
+                    fcfs_cpu.cpu_status()
+                    print("STARTING TO COMPILE DATA...")
+                    fcfs_cpu.compile()
 
-            if confirmation == "NO":
-                break
-            else:
-                print("YOU WISHED TO CONTINUE")
+                    # Compile end: Display Results.
+                    print("STATUS OF THE CPU AFTER COMPILING")
+                    fcfs_cpu.cpu_status()
 
+                    # Choice if they want to redo it again.
+                    confirmation = str(input("DO YOU WISH TO RE-ENTER VALUES AND COMPILE IT AGAIN? ENTER YES OR NO: "))
+
+                    if confirmation == "NO":
+                        break
+                    else:
+                        print("YOU WISHED TO CONTINUE")
+
+                else:
+                    print("0 IS NOT ALLOWED FOR NUMBER OF PROCESSES.")
+
+        # CASE 2.2: Round Robin Algorithm.
         elif algo_choice is 2:
-            quantum_time = int(input("ENTER THE QUANTUM TIME: "))
-            round_robin_cpu = RoundRobin(quantum_time=quantum_time)
-            print("STARTING UP THE CPU.....")
-            round_robin_cpu.startup_cpu(no=no_of_processes)
 
-            print("INITIALIZE ARRIVAL TIMES: ")
-            for i in range(0, no_of_processes):
-                time = int(input("ENTER ARRIVAL TIME FOR PROCESS P{} :".format(i)))
-                init_arrival_time.append(time)
+            # Loop for keeping the algorithm alive if user wants to.
+            while True:
+                quantum_time = int(input("ENTER THE QUANTUM TIME: "))
+                round_robin_cpu = RoundRobin(quantum_time=quantum_time)
+                print("STARTING UP THE CPU.....")
+                round_robin_cpu.startup_cpu(no=no_of_processes)
 
-            print("INITIALIZE BURST TIMES: ")
-            for i in range(0, no_of_processes):
-                time = int(input("ENTER BURST TIME FOR PROCESS P{} :".format(i)))
-                init_burst_time.append(time)
+                if no_of_processes is not 0:
 
-            round_robin_cpu.init_burst_time(burst_times=init_burst_time)
-            round_robin_cpu.init_arrival_time(arrival_times=init_arrival_time)
+                    # Arrival Times Input.
+                    print("INITIALIZE ARRIVAL TIMES: ")
+                    for i in range(0, no_of_processes):
+                        time = int(input("ENTER ARRIVAL TIME FOR PROCESS P{} :".format(i)))
+                        init_arrival_time.append(time)
 
-            round_robin_cpu.cpu_status()
-            print("STARTING TO COMPILE DATA...")
-            round_robin_cpu.compile()
+                    # Burst Times Input.
+                    print("INITIALIZE BURST TIMES: ")
+                    for i in range(0, no_of_processes):
+                        time = int(input("ENTER BURST TIME FOR PROCESS P{} :".format(i)))
+                        init_burst_time.append(time)
 
-            print("STATUS OF THE CPU AFTER COMPILING")
-            round_robin_cpu.cpu_status()
+                    round_robin_cpu.init_burst_time(burst_times=init_burst_time)
+                    round_robin_cpu.init_arrival_time(arrival_times=init_arrival_time)
 
-            confirmation = str(input("DO YOU WISH TO RE-ENTER VALUES AND COMPILE IT AGAIN? ENTER YES OR NO: "))
+                    # Compile Start.
+                    round_robin_cpu.cpu_status()
+                    print("STARTING TO COMPILE DATA...")
+                    round_robin_cpu.compile()
 
-            if confirmation == "NO":
-                break
-            else:
-                print("YOU WISHED TO CONTINUE")
+                    # Compile End: Display Results.
+                    print("STATUS OF THE CPU AFTER COMPILING")
+                    round_robin_cpu.cpu_status()
 
+                    confirmation = str(input("DO YOU WISH TO RE-ENTER VALUES AND COMPILE IT AGAIN? ENTER YES OR NO: "))
+
+                    if confirmation == "NO":
+                        break
+                    else:
+                        print("YOU WISHED TO CONTINUE")
+
+                else:
+                    print("0 IS NOT ALLOWED FOR NUMBER OF PROCESSES.")
+
+        # ERROR CASE
         else:
             print("WRONG INPUT. TRY AGAIN")
 
+    # ERROR CASE
     else:
         print("WRONG INPUT. TRY AGAIN")
 
